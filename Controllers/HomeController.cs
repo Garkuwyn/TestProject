d2a344d30ec024c8f2a3160036a027e200b691a6
Garkuwyn <taryaletter@inbox.ru>
2014-04-12 20:22:37 +0300

    New project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GraduatesProject.DAL;
+namespace GraduatesProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private GraduatesEntitiesModel graduatesEntities;
+        public HomeController()
+        {
+            graduatesEntities = new GraduatesEntitiesModel();
+        }
+        public ActionResult Index()
+        {
+            var students = graduatesEntities.Persons.ToList();
+            ViewBag.Message = "Измените этот шаблон, чтобы быстро приступить к работе над приложением ASP.NET MVC.";
+
+            return View(students);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Страница описания приложения.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Страница контактов.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GraduatesProject.MvcApplication" Language="C#" %>
